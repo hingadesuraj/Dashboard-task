8c233dafa4301465d0045a5b98a7bcab015d4d83
Suraj <hingadesuraj@gmail.com>
2024-03-25 17:29:48 +0530

    Setup Dashbaord project environment

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-blue-50 font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
