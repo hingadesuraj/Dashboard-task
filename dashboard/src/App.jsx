4048b80bc50ff8054dadfac28f1d221f25b78d40
Suraj <hingadesuraj@gmail.com>
2024-03-25 17:44:35 +0530

    Impliment Router using react-router-dom package

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Dashboard from "./pages/Dashboard";
+import CreateNews from "./pages/CreateNews";
+import ManageNews from "./pages/ManageNews";
+import PerformanceReportPage from "./pages/PerformanceReportPage";
 
 function App() {
   return (
     <>
-      <div>
-        <h1 className=" font-bold text-gray-600 ">Hello</h1>
-      </div>
+     <BrowserRouter>
+     <h1>hello</h1>
+     <Routes>
+       <Route path="/" element={<Dashboard />} />
+       <Route path="/create" element={<CreateNews />} />
+       <Route path="/allnews" element={<ManageNews />} />
+       <Route path="/report" element={<PerformanceReportPage />} />
+     </Routes>
+     </BrowserRouter>
     </>
   );
 }
